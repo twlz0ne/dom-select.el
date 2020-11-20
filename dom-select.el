558c86db9decd1671d1bfeb52b22adf294658cdd
Gong Qijian <gongqijian@gmail.com>
2020-11-21 00:00:10 +0800

    Init commit

---
+++ dom-select.el
@@ -0,0 +1,38 @@
+;;; dom-select.el --- Dom select -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020 Gong Qijian <gongqijian@gmail.com>
+
+;; Author: Gong Qijian <gongqijian@gmail.com>
+;; Created: 2020/11/20
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "24.4"))
+;; URL: https://github.com/twlz0ne/dom-select.el
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; DOM select
+
+;;; Change Log:
+
+;;  0.1.0  2020/11/20  Initial version.
+
+;;; Code:
+
+
+(provide 'dom-select)
+
+;;; dom-select.el ends here
